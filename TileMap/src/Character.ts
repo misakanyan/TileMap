0063051604ea47e9a9180f0496db5ddd7872de2a
lenovo\lenovo-pc <2213286021@qq.com>
2016-11-02 00:29:25 +0900

    素材替换，人物动画修正

--- TileMap/src/Character.ts
+++ TileMap/src/Character.ts
@@ -12,14 +12,16 @@ class Character extends egret.DisplayObjectContainer {
         super();
         this._main = main;
         this._body = new egret.Bitmap;
-        this._body.texture = RES.getRes("3_png");
+        this._body.texture = RES.getRes("chara1_png");
         this._main.addChild(this._body);
+        this._body.width = 50;
+        this._body.height = 50;
         this._body.anchorOffsetX = this._body.width * 1;
-        console.log("anchorx :"+this._body.anchorOffsetX);
+        console.log("anchorx :" + this._body.anchorOffsetX);
         this._body.anchorOffsetY = this._body.height * 0;
         this._stateMachine = new StateMachine();
-        this._body.x = this._main.stage.stageWidth;
-        this._body.y = this._main.stage.stageHeight*0;
+        this._body.x = this._main.stage.stageWidth * 0.1;
+        this._body.y = this._main.stage.stageHeight * 0.9;
         console.log(this._body.x);
         this._ifidle = true;
         this._ifmove = false;
@@ -44,11 +46,13 @@ class Character extends egret.DisplayObjectContainer {
             }
             this.startMove();
             //egret.Tween.get(this._body).to({ x: targetX, y: targetY }, 2000, egret.Ease.sineInOut).call(function () { this.idle() }, this);
-            var timer: egret.Timer = new egret.Timer(500, path.length-1);
+            var timer: egret.Timer = new egret.Timer(500, path.length - 1);
             timer.addEventListener(egret.TimerEvent.TIMER, function (e: egret.TimerEvent): void {
-                egret.Tween.get(this._body).to({ x: (path[timer.currentCount].x+1) * 50, y: (path[timer.currentCount].y) * 50 },400,egret.Ease.sineInOut).call(function(){this.idle()},this);
-                
-                console.log("target:"+path[timer.currentCount-1].x +" , "+ path[timer.currentCount-1].y);
+                egret.Tween.get(this._body).to({ x: (path[timer.currentCount].x + 1) * 50, y: (path[timer.currentCount].y) * 50 }, 400, egret.Ease.sineInOut);
+                console.log("target:" + path[timer.currentCount - 1].x + " , " + path[timer.currentCount - 1].y);
+            }, this);
+            timer.addEventListener(egret.TimerEvent.TIMER_COMPLETE, function (e: egret.TimerEvent): void {
+                this.idle();
             }, this);
             timer.start();
             console.log(path.length);
@@ -69,15 +73,15 @@ class Character extends egret.DisplayObjectContainer {
 
     //播放运动动画
     public startMove() {
-        var list = ["chara1_png", "chara2_png", "chara3_png", "chara4_png", "chara5_png", 
-        "chara6_png", "chara7_png", "chara8_png", "chara9_png", "chara10_png", "chara11_png", 
-        "chara12_png", "chara13_png", "chara14_png", "chara15_png", "chara16_png", "chara17_png", 
-        "chara18_png", "chara19_png", "chara20_png", "chara21_png", "chara22_png", "chara23_png", 
-        "chara24_png", "chara25_png", "chara26_png"];
+        var list = ["chara1_png", "chara2_png", "chara3_png", "chara4_png", "chara5_png",
+            "chara6_png", "chara7_png", "chara8_png", "chara9_png", "chara10_png", "chara11_png",
+            "chara12_png", "chara13_png", "chara14_png", "chara15_png", "chara16_png", "chara17_png",
+            "chara18_png", "chara19_png", "chara20_png", "chara21_png", "chara22_png", "chara23_png",
+            "chara24_png", "chara25_png", "chara26_png"];
         var count = -1;
-this._body.texture = RES.getRes("3_png");
+        //this._body.texture = RES.getRes("3_png");
         //循环执行
-        /*egret.Ticker.getInstance().register(() => {
+        egret.Ticker.getInstance().register(() => {
 
             if (this._ifmove) {
                 count = count + 0.5;
@@ -88,13 +92,13 @@ this._body.texture = RES.getRes("3_png");
                 this._body.texture = RES.getRes(list[Math.floor(count)]);
             }
 
-        }, this);*/
+        }, this);
 
     }
 
     public startidle() {
 
-        this._body.texture = RES.getRes("3_png");
+        this._body.texture = RES.getRes("chara11_png");
 
     }
 
